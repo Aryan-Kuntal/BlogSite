2650ecff6979cb53b36bcf63cfe83f3fc584abd6
Raj Aryan <rajaryankuntal@gmail.com>
2022-03-08 01:34:35 +0530

    second commit

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY','1$2^g6+&/53F4314^@gth+2g$u%fr^j5^g)ga@g+f{t}h%w(d%')
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
